8ea2434332297faae6814acea7664750f8174e40
Anykey <antoman1994@gmail.com>
2016-03-30 12:54:27 +0300

    Refactoring

--- cudload.sh
+++ cudload.sh
@@ -1,25 +1,69 @@
 #!/bin/bash
+#
+# Cudload - Crypted up- down- load
+#
+#  Yandex.Disk
+#  Google Drive
+#  Dropbox
+#
+
 VERSION=1.0
 
-login=test	  #login yandex
-password=test	#password yandex
+# Yandex access
+login=srav.ja.na
+password=223355668899sSs
 
-DROPBOX_UPLOADER_PATH="/usr/local/Cudload/"
+SCRIPT_PATH=`pwd`
+
+DROPBOX_UPLOADER_PATH="${SCRIPT_PATH}/dropbox/"
 DROPBOX_API_SCRIPT="https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh"
 
-PASSWORD_PATH="/usr/local/Cudload/"
-BACKUP_PATH="/usr/local/Cudload/"
+PASSWORD_PATH="${SCRIPT_PATH}/"
+BACKUP_PATH="${SCRIPT_PATH}/backup/"
 
-DOWNLOAD_PATH="/var/download/"
-ARCHIVE_BACKUP_PATH="/var/Cudload/"
+DOWNLOAD_PATH="${SCRIPT_PATH}/download/"
+ARCHIVE_BACKUP_PATH="${SCRIPT_PATH}/archive/"
 
-GOOGLE_UPLOADER_PATH="/usr/local/Cudload/"
+GOOGLE_UPLOADER_PATH="${SCRIPT_PATH}/google/"
 GOOGLE_API_SCRIPT="https://drive.google.com/uc?id=0B3X9GlR6EmbnSnVqdUFWSjJhbUU"
-SCRIPT_PATH="/usr/local/Cudload/"
+
 
 DATE=`date +%Y-%m-%d`
 CURL=`which curl`
 WGET=`which wget`
+
+if [ ! -d  ${DROPBOX_UPLOADER_PATH} ]; then
+ mkdir ${DROPBOX_UPLOADER_PATH}
+fi;
+
+if [ ! -d  ${GOOGLE_UPLOADER_PATH} ]; then
+ mkdir ${GOOGLE_UPLOADER_PATH}
+fi;
+
+if [ ! -d  ${BACKUP_PATH} ]; then
+ mkdir ${BACKUP_PATH}
+fi;
+
+if [ ! -d  ${DOWNLOAD_PATH} ]; then
+ mkdir ${DOWNLOAD_PATH}
+fi;
+
+if [ ! -d  ${ARCHIVE_BACKUP_PATH} ]; then
+ mkdir ${ARCHIVE_BACKUP_PATH}
+fi;
+
+if [ ! -d  ${SCRIPT_PATH} ]; then
+ mkdir ${SCRIPT_PATH}
+fi;
+
+if [ ! -d ${PASSWORD_PATH} ]; then
+  mkdir ${PASSWORD_PATH};
+fi;
+
+if [ ! -e ${PASSWORD_PATH}/pass.txt ]; then
+  echo "super secret plain text password ${DATE}" > ${PASSWORD_PATH}/pass.txt
+  chmod 600 ${PASSWORD_PATH}/pass.txt
+fi;
 
 #********************************************
 #  Usage
@@ -34,15 +78,30 @@ echo "
      -i - interactive mode
      -u - upload mode
      -d - download mode
-     -r - dropbox
-     -y - yandex
-     -g - google
-     -w - get dropbox uloader
+     -r - Dropbox
+     -y - Yandex.Disk
+     -g - Google Drive
+     -w - get Dropbox uloader
      -q - get google uploader
      -m - archive for last file
      -o - archive for file or folder
      -l - list of files
+     -f - name of file to download
+     -z - ID for file in Google Drive
      -h - Show this help
+
+   Examples:
+     Show list of files on Google Drive
+       ./cudload.sh -l -g
+
+     Encrypt and upload /home/administrator to Google Drive
+       ./cudload.sh -u -g -o /home/administrator/
+
+     Download and decrypt file from Google Drive by ID 0BzA2l61Ik_23VzZtT0NXdDJYcW8
+       ./cudload.sh -d -g -f 2016-03-30 -z 0BzA2l61Ik_23VzZtT0NXdDJYcW8
+
+     Download from Yandex.Disk
+       ./cudload.sh -d -y -f 2016-03-30
 "
 
 exit 1;
@@ -55,7 +114,7 @@ exit 1;
 dropbox_uploader () {
 
 	${WGET} -P ${DROPBOX_UPLOADER_PATH} ${DROPBOX_API_SCRIPT}
-	chmod +x ${DROPBOX_UPLOADER_PATH} dropbox_uploader.sh
+	chmod 700 ${DROPBOX_UPLOADER_PATH} dropbox_uploader.sh
 	echo "Downloaded Dropbox uploader";
 
 }
@@ -66,7 +125,7 @@ dropbox_uploader () {
 google_uploader () {
 
 	${WGET} ${GOOGLE_API_SCRIPT} -O ${GOOGLE_UPLOADER_PATH}google_uploader
-	chmod +x ${GOOGLE_UPLOADER_PATH}google_uploader
+	chmod 700 ${GOOGLE_UPLOADER_PATH}google_uploader
 
 }
 
@@ -77,6 +136,11 @@ make_archive () {
 
   if ! [ -d "${ARCHIVE_BACKUP_PATH}" ]; then
 		mkdir ${ARCHIVE_BACKUP_PATH}
+	fi;
+
+	if [ ! -e $BACKUP_PATH ];then
+	  echo "File or dir does not exists";
+	   exit 1;
 	fi;
 
   if [ "${TYPE_ARCHIVE}"  = "m" ]; then
@@ -101,13 +165,18 @@ make_archive () {
 # Decrypt and remove crypted archive
 #********************************************
 destroy_archive () {
-	echo -n "Path to file for decrypting:" ;
-  read FILENAME
+
+  if [ ! ${DOWNLOAD_PATH}${FILENAME} ];then
+	  echo -n "Path to file for decrypting:" ;
+    read FILENAME
+  fi;
+
 	openssl enc -d -aes-256-cbc -in ${DOWNLOAD_PATH}${FILENAME}.tar.gz.code -out ${DOWNLOAD_PATH}${FILENAME}.tar.gz -pass file:${PASSWORD_PATH}pass.txt
 
 	rm ${DOWNLOAD_PATH}${FILENAME}.tar.gz.code
 
 	echo "Done";
+	echo "Decrypted archive: ${DOWNLOAD_PATH}${FILENAME}.tar.gz ";
 }
 
 #********************************************
@@ -148,6 +217,11 @@ dropbox_download () {
 #****************************************************
 yandex_upload () {
 
+  if [ ! -e ${ARCHIVE_BACKUP_PATH}${DATE}.tar.gz.code ];then
+    echo " !!! File does not exists: ${ARCHIVE_BACKUP_PATH}${DATE}.tar.gz.code";
+    exit 1;
+  fi;
+
 	${CURL} --user ${login}:${password} -T ${ARCHIVE_BACKUP_PATH}${DATE}.tar.gz.code "https://webdav.yandex.ru"
 	echo "File upload"
 
@@ -157,12 +231,17 @@ yandex_upload () {
 # Download from Yandex.Disk
 #*****************************************************
 yandex_download () {
-	echo 'Enter file name to download from Yandex Disk:';
-	read FILE
+  if [ ! ${FILE_DOWNLOAD_NAME} ];then
+	  echo 'Enter file name to download from Yandex Disk:';
+	  read FILENAME
+	else
+	  FILENAME=${FILE_DOWNLOAD_NAME};
+	fi;
 
-	${CURL} --user ${login}:${password}  "https://webdav.yandex.ru/${FILE}.tar.gz.code" -o ${DOWNLOAD_PATH}${FILE}.tar.gz.code
-	echo "Download from Yandex Disk completed"
+	FILENAME=`echo ${FILENAME} | sed 's/.tar.gz.*//'`;
 
+	${CURL} --user ${login}:${password}  "https://webdav.yandex.ru/${FILENAME}.tar.gz.code" -o ${DOWNLOAD_PATH}${FILENAME}.tar.gz.code
+	echo "Download from Yandex Disk completed";
 }
 
 #*****************************************
@@ -175,41 +254,63 @@ google_upload () {
 	  exit 1;
 	fi;
 
-
 	if [ ! -f ${GOOGLE_UPLOADER_PATH}google_uploader ]; then
 		google_uploader
 	fi;
 
-
 	${GOOGLE_UPLOADER_PATH}google_uploader upload -f ${ARCHIVE_BACKUP_PATH}${DATE}.tar.gz.code
 	echo "File upload"
 	rm ${ARCHIVE_BACKUP_PATH}${DATE}.tar.gz.code
-	echo "Coded archive deleted"
-
-
+	echo "Crypted archive deleted"
 }
 
 #******************************************
 #Скачиваем с Google Drive
 #******************************************
 google_download () {
-	echo 'Enter file name to download from Google Drive';
-	read FILENAME;
-	${GOOGLE_UPLOADER_PATH}google_uploader list -t $FILENAME;
 
-	# TODO : Grep ID
+  if [ ! -e  ${GOOGLE_UPLOADER_PATH}google_uploader ]; then
+    google_uploader;
+  fi;
 
-	echo 'Enter file ID to download';
-  read ID;
+  if [ ! ${FILE_DOWNLOAD_NAME} ];then
+	  echo
+	  echo ' # Enter file name to download from Google Drive';
+	  read FILENAME;
+	else
+	  FILENAME=${FILE_DOWNLOAD_NAME};
+	fi
+
+  if [ -e ${FILENAME} ];then
+    rm ${FILENAME};
+  fi;
+
+	FILENAME=`echo ${FILENAME} | sed 's/.tar.gz.*//'`;
+
+  if [ ! ${FILE_DOWNLOAD_ID} ]; then
+    ${GOOGLE_UPLOADER_PATH}google_uploader list -t ${FILENAME};
+
+  	echo
+  	echo ' # Enter ID of file to download';
+    read ID;
+  else
+    ID=${FILE_DOWNLOAD_ID}
+  fi
 
 	#ID=`${GOOGLE_UPLOADER_PATH}google_uploader list -t $FILE | grep ${FILE} | awk '{ print $1 }'`
 	FILE=${ID}
 
-	${GOOGLE_UPLOADER_PATH}google_uploader download  -i $FILE
-	cp ${SCRIPT_PATH}$FILENAME.tar.gz.code ${DOWNLOAD_PATH}
-	rm ${SCRIPT_PATH}$FILENAME.tar.gz.code
+  ${GOOGLE_UPLOADER_PATH}google_uploader download  -i ${FILE}
 
-	echo 'Download from Google Drive has been completed';
+  if [ ! -e ${FILENAME}.tar.gz.code ]; then
+    echo ' !!! Error occured while downloading from Google Drive';
+	  exit 1
+  fi;
+
+	cp ${FILENAME}.tar.gz.code ${DOWNLOAD_PATH}
+  rm ${FILENAME}.tar.gz.code
+
+	echo ' # Download from Google Drive has been completed';
 }
 
 #*******************************************
@@ -217,15 +318,20 @@ google_download () {
 #*******************************************
 file_list () {
 
-	echo -np "Select system for $ACTION file: r - dropbox, y - yandex, g - google : " SYSTEM
+  if [ ! "${SYSTEM}" ];then
+
+  	echo -n "Select system for $ACTION file: r - dropbox, y - yandex, g - google : "
+    read SYSTEM;
+
+  fi;
 
 	if [ "${SYSTEM}" = r ]; then
 		${DROPBOX_UPLOADER_PATH}dropbox_uploader.sh list
-	fi;
-
-	if [ "${SYSTEM}" = g ]; then
+	elif [ "${SYSTEM}" = g ]; then
 	  ${GOOGLE_UPLOADER_PATH}google_uploader list
-	fi;
+	else
+	  echo " !!! System does not supports list";
+  fi;
 
 }
 
@@ -256,7 +362,7 @@ interactive () {
 	fi;
 }
 
-while getopts "udirywgqm:o:l" opt ; do
+while getopts "udirywgqm:o:l:f:z:" opt ; do
   case "$opt" in
 
   w)
@@ -295,6 +401,13 @@ while getopts "udirywgqm:o:l" opt ; do
   l)
   		ACTION=l;
   ;;
+  f)
+     FILE_DOWNLOAD_NAME=$OPTARG;
+  ;;
+  z)
+     FILE_DOWNLOAD_ID=$OPTARG;
+  ;;
+
 esac
 done
 
